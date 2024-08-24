7fd09bbfd3b8282fa074aee7eb59f987a13ab0dd
devr77 <draj56699@zoho.com>
2024-08-24 10:47:36 +0530

    fix:prisma-error

--- src/app/api/cars/route.js
+++ src/app/api/cars/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 // To handle a GET request to /api
 export async function GET(request) {
-  const cars = await prisma.Post.findMany();
+  const cars = await prisma?.Post?.findMany();
 
   return NextResponse.json({ cars }, { status: 200 });
 }
