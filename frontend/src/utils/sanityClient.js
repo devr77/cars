3168a24e4bc902effe3047779a3a6ccfb223e72d
devr77 <draj56699@zoho.com>
2024-09-23 01:03:54 +0530

    model

--- frontend/src/utils/sanityClient.js
+++ frontend/src/utils/sanityClient.js
@@ -4,7 +4,7 @@ export const client = createClient({
   projectId: "pcxwdqmb",
   dataset: "production",
   apiVersion: "2024-04-16",
-  useCdn: false,
+  useCdn: true,
   token:
     "skF7zkOmqJVCuXIvgLHzm36Y1uzNfx3FHWJKSpCYRnXq3t5VIH1PkUyGjxqke8D6NMZajoxwlNPVK35ac16nLCwtFV8DkJufxRAHOk6cYtxMmvyLgR94tjaJAtff3Ls4HzBySFtgRIQ2RLpbg4thM3B1sB9kOisQRmv56W8MxLjsbHO7oOsE",
 });
