dcf7e4f2b755290cc3427580e0fe6f586834d891
devr77 <draj56699@zoho.com>
2024-09-23 19:26:47 +0530

    sanity:version

--- frontend/src/utils/sanityClient.js
+++ frontend/src/utils/sanityClient.js
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: "pcxwdqmb",
   dataset: "production",
-  // apiVersion: "2024-09-09",
+  apiVersion: "2024-09-09",
   useCdn: false,
   token:
     "skF7zkOmqJVCuXIvgLHzm36Y1uzNfx3FHWJKSpCYRnXq3t5VIH1PkUyGjxqke8D6NMZajoxwlNPVK35ac16nLCwtFV8DkJufxRAHOk6cYtxMmvyLgR94tjaJAtff3Ls4HzBySFtgRIQ2RLpbg4thM3B1sB9kOisQRmv56W8MxLjsbHO7oOsE",
