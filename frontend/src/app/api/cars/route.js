5138433892287cd9cd819c4c7be42f79d25aeb71
devr77 <draj56699@zoho.com>
2024-09-30 14:21:08 +0530

    prisma:removed

--- frontend/src/app/api/cars/route.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-// To handle a GET request to /api
-export async function GET(request) {
-  const cars = await prisma?.Post?.findMany();
-
-  return NextResponse.json({ cars }, { status: 200 });
-}
